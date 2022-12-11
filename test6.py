e8caf309bdfcac97a8ffa88950355b73cceec9e5
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-11 13:35:44 +0100

    tdd

---
+++ test6.py
@@ -0,0 +1,9 @@
+from test7 import *
+
+def test_0():
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(4) == 4
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(15) == "FizzBuzz"
